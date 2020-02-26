68ca609dfaf8bfa2f9bd2db4f33de5a8c072ae88
MykolaV <kolya.vasevich@gmail.com>
2020-02-26 17:58:05 +0200

    Created one controller and two actions with views

---
+++ Web_Lab_1/Controllers/NotDefaultController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web_Lab_1.Controllers
+{
+    public class NotDefaultController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public NotDefaultController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        
+        // GET
+        public IActionResult SomeAction()
+        {
+            return View();
+        }
+
+        public IActionResult ViewWithGetParam([FromQuery] string from)
+        {
+            string model = $"Get parameter was \"{from}\"";
+            return View("ViewWithGetParam", model);
+        }
+    }
+}
