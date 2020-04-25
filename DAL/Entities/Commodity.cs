362ac8606dff9fcfa792caf547408a8dcb05934e
MykolaV <kolya.vasevich@gmail.com>
2020-04-25 19:35:32 +0300

    removed useless property in dal entity

--- DAL/Entities/Commodity.cs
+++ DAL/Entities/Commodity.cs
@@ -5,7 +5,6 @@ namespace DAL.Entities
     public class Commodity : BaseEntity
     {
         public string Name { get; set; }
-        public int Units { get; set; }
         public int Price { get; set; }
         public IEnumerable<OrdersCommodities> Orders { get; set; }
     }
