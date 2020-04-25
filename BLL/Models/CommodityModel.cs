c28ec09f93ed5e6935c63aee2b163c4bead65cc7
MykolaV <kolya.vasevich@gmail.com>
2020-04-25 19:31:45 +0300

    removed useless property

--- BLL/Models/CommodityModel.cs
+++ BLL/Models/CommodityModel.cs
@@ -4,7 +4,6 @@ namespace BLL.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Units { get; set; }
         public int Price { get; set; }
     }
 }
